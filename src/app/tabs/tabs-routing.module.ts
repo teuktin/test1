9a0a8065bdcc2a37317b7759b6d87fd9a07fe46e
valent.212116149@gmail.com <d0n9b4n9>
2022-03-15 16:04:50 +0700

    Added my project

---
+++ src/app/tabs/tabs-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsPage,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () => import('../home/home.module').then( m => m.HomePageModule)
+      },
+      {
+        path: 'my-cart',
+        loadChildren: () => import('../my-cart/my-cart.module').then( m => m.MyCartPageModule)
+      },
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabsPageRoutingModule {}
